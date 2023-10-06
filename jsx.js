06e12e7c4e99cc91bbde910aca7f9ad8b1acae5a
Enoque <thesonlike@gmail.com>
2023-10-05 23:43:38 -0300

    commit

--- jsx.js
+++ jsx.js
@@ -1,1 +1,5 @@
 //writing jsx for defining components
+//
+//JSX is a syntax extension for JavaScript that 
+//lets you write HTML-like markup inside a JavaScript file.
+// Although there are other ways to write components,
