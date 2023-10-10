c2c680a5f457969e470c60a1517d0e2dde951cbd
Enoque <thesonlike@gmail.com>
2023-10-10 05:08:36 -0300

    commit

--- jsx.js
+++ jsx.js
@@ -1,5 +1,13 @@
-//writing jsx for defining components
-//
-//JSX is a syntax extension for JavaScript that 
-//lets you write HTML-like markup inside a JavaScript file.
-// Although there are other ways to write components,
+//destructuring is kinda doing Array accessing but 
+//creating a variable and retrieving the specific data from the array
+const numbers = [0,1,2,3,4,5,6];
+
+// const letters = [lol,sal,dolor,stiff];
+
+const [a,b,c, ...rest] = numbers;
+
+const arrayAccess = a[1];
+
+console.log(a);
+
+//"a" its not a variable, understand this bs latter.
