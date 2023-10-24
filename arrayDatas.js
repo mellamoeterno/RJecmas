5228c476e2fe90de7742a00c61617879908afca0
Enoque <thesonlike@gmail.com>
2023-10-24 18:49:16 -0300

    commit

--- arrayDatas.js
+++ arrayDatas.js
@@ -6,9 +6,17 @@ const numbers = [0,1,2,3,4,5,6];
 
 const [a,b,c, ...rest] = numbers;
 
-const arrayAccess = a[1];
+//this is assigning the value from inside the other variable above, 'numbers' to the newly created variables inside
+//the brackets respectively it seems like.
 
-console.log(a);
+//if in doubt in the future just change the value rom the console.log, it'll show the change from the elements from 
+//the variable 'numbers' being assigned to the variables inside the brackets.
+
+//not sure about how the ...rest works, which is a parameter.
+
+const arrayAccess = a;
+
+console.log(b);
 
 //"a" its not a variable, understand this bs latter.
 
